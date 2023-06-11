82049496cc300dc2f29c4fe14b687c55ca000022
FranMontesSegales <95092587+FranMontesSegales@users.noreply.github.com>
2023-06-12 00:56:52 +0200

    prueba

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const cors = require('cors');
 const loaders = require('./loaders');
 const config = require('./config');
 const { logger } = require('./utils');
 
 const app = express();
+
+// Habilitar CORS
+app.use(cors());
 
 loaders.init(app, config);
 
